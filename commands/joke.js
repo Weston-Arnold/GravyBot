553b40134a635210094a31ecd458ee4d855cf8fc
Weston Arnold <tallonxvii@gmail.com>
2023-01-26 20:33:15 -0600

    wip:
    add axios
    add joke GET request and return to caller

---
+++ commands/joke.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("joke")
+    .setDescription("Tells a random joke"),
+  async execute(interaction) {
+    const { data } = await axios({
+      method: "GET",
+      url: "https://v2.jokeapi.dev/joke/Any",
+    });
+
+    //1 part joke
+    if (!!data.joke) {
+      await interaction.reply(data.joke);
+      return;
+    }
+
+    //2 part joke
+    await interaction.reply(`${data.setup} \n${data.delivery}`);
+  },
+};
