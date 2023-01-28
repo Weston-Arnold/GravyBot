8ed3f8c5f499473d30fa0482f79e810f98db080f
Weston Arnold <tallonxvii@gmail.com>
2023-01-27 23:46:18 -0600

    Feat: Joke API Integration (#7)
    * wip:
    
    add axios
    add joke GET request and return to caller
    
    * default nsfw blacklist
    
    * feat: joke api integration complete
    
    * Enforce safe mode by default

---
+++ commands/joke.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("joke")
+    .setDescription("Replies with a joke")
+    .addStringOption((option) =>
+      option
+        .setName("category")
+        .setDescription("The category of the joke")
+        .setRequired(true)
+        .addChoices(
+          { name: "Any", value: "Any" },
+          { name: "Programming", value: "Programming" },
+          //To enabled this, "safe-mode" must be removed from the endpoint below
+          // { name: "Dark (NSFW)", value: "Dark"}
+          { name: "Pun", value: "Pun" },
+          { name: "Spooky", value: "Spooky" },
+          { name: "Christmas", value: "Christmas" }
+        )
+    ),
+  async execute(interaction) {
+    const category =
+      interaction.options.getString("category") ?? "No category provided";
+
+    const { data } = await axios({
+      method: "GET",
+      //Test API: https://v2.jokeapi.dev/
+      url: `https://v2.jokeapi.dev/joke/${category}?safe-mode`,
+    });
+
+    if (data.error) {
+      console.log(data);
+      await interaction.reply(
+        `**API ERROR**: ${
+          data.message ?? "Unknown Reason"
+        } \n\nAdditional Info: ${data.additionalInfo ?? "N/A"}`
+      );
+      return;
+    }
+
+    //1 part joke
+    if (!!data.joke) {
+      await interaction.reply(data.joke);
+      return;
+    }
+
+    //2 part joke
+    await interaction.reply(`${data.setup} \n${data.delivery}`);
+  },
+};
